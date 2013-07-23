f383fa7d9ae7ac4f6a70e09048d1416b56956e11
Ryan McCue <me@ryanmccue.info>
2013-07-23 17:52:19 +1000

    Add example configuration

---
+++ tests/config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+// Set the URL to your API installation
+$testconfig['url'] = 'http://example.com/wp-json.php';
+
+// Username to use
+// Set to blank to use unauthenticated API
+$testconfig['username'] = 'admin';
+
+// Password for the above user
+$testconfig['password'] = 'password';
--- tests/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$testconfig = array();
-$testconfig['url'] = 'http://localhost/gsoc/wp-json.php';
-$testconfig['username'] = 'admin';
-$testconfig['password'] = 'password';
