f8773a97b6c5dfa6dca0b4ab3720f06c92f8df22
Ryan McCue <me@ryanmccue.info>
2014-05-26 17:00:39 +1000

    Add user get/create methods

--- library/WPAPI/Users.php
+++ library/WPAPI/Users.php
@@ -36,7 +36,6 @@ class WPAPI_Users implements WPAPI_Collection {
 	 * @return array List of WPAPI_User objects
 	 */
 	public function getAll() {
-
 		$response = $this->api->get( WPAPI::ROUTE_USERS );
 		$users = json_decode( $response->body, true );
 		foreach ( $users as &$user ) {
@@ -45,4 +44,48 @@ class WPAPI_Users implements WPAPI_Collection {
 		return $users;
 	}
 
+	/**
+	 * Get a single user
+	 *
+	 * @throws Requests_Exception Failed to retrieve the user
+	 * @throws Exception Failed to decode JSON
+	 * @param int $id User ID
+	 * @return WPAPI_User
+	 */
+	public function get($id) {
+		$url = sprintf( WPAPI::ROUTE_USER, $id );
+		$response = $this->api->get( $url );
+		$response->throw_for_status();
+
+		$data = json_decode( $response->body, true );
+
+		$has_error = ( function_exists('json_last_error') && json_last_error() !== JSON_ERROR_NONE );
+		if ( ( ! $has_error && $data === null ) || $has_error ) {
+			throw new Exception( $response->body );
+		}
+
+		return new WPAPI_User( $this->api, $data );
+	}
+
+	/**
+	 * Create a new user
+	 *
+	 * @throws Requests_Exception Failed to retrieve the user
+	 * @throws Exception Failed to decode JSON
+	 * @param array $data User data to create
+	 * @return WPAPI_User
+	 */
+	public function create( $data ) {
+		$data = json_encode( $data );
+		$headers = array( 'Content-Type' => 'application/json' );
+		$response = $this->api->post( WPAPI::ROUTE_USERS, $headers, $data );
+		$response->throw_for_status();
+
+		$data = json_decode( $response->body, true );
+		$has_error = ( function_exists('json_last_error') && json_last_error() !== JSON_ERROR_NONE );
+		if ( ( ! $has_error && $data === null ) || $has_error ) {
+			throw new Exception( $response->body );
+		}
+		return new WPAPI_User( $this->api, $data );
+	}
 }
